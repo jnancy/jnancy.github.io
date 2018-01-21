6b800f8cd896e561b741b742c0dae418a8c7963b
Nancy Jiang <nancyjiang10@gmail.com>
2018-01-20 16:17:37 -0800

    more updates

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var options = {
+  strings: ["<h3>Computer Engineering Student.</h3>", "<h3>Web Developer.</h3>"],
+  typeSpeed: 50,
+  backSpeed: 40,
+  loop: true,
+  loopCount: Infinity,
+  showCursor: true,
+  cursorChar: '|',
+  autoInsertCss: true
+}
+
+var typed = new Typed(".element", options);
