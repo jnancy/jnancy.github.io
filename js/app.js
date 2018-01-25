5eeac08bb4a81adf87c852e165115095b0d133e6
Nancy Jiang <nancyjiang10@gmail.com>
2018-01-24 19:43:30 -0800

    added project deets

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#EEE"
+        "value": "#EFEFEF"
       },
       "shape": {
         "type": "circle",
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#444",
+        "color": "#333",
         "opacity": 0.4,
         "width": 1
       },
