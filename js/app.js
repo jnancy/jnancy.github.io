87bfed14551a83dfa4b87aba36cc2add467c34df
Nancy Jiang <nancyjiang10@gmail.com>
2018-01-25 15:55:17 -0800

    dark theme

--- js/app.js
+++ js/app.js
@@ -23,13 +23,13 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#EFEFEF"
+        "value": "#111"
       },
       "shape": {
         "type": "circle",
         "stroke": {
           "width": 0,
-          "color": "#555"
+          "color": "#FFF"
         },
         "polygon": {
           "nb_sides": 5
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#555",
+        "color": "#FFF",
         "opacity": 0.4,
         "width": 1
       },
